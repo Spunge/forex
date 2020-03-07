15aa92cc578455ec9c46e219631558ded00974cb
root <root@root>
2020-03-07 19:12:02 +0100

    make notes output by forex last a beat

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,18 @@ use gilrs::{Gilrs, Event};
 use std::time::SystemTime;
 use std::io;
 
-/*
- * Midi message representation
- */
-#[derive(Debug, Copy, Clone)]
-struct Message {
-    time: SystemTime,
-    channel: u8,
-    note: u8,
-    velocity: u8,
+#[derive(Debug)]
+struct Hit {
+    tom_id: u8,
+    velocity: f32,
 }
 
-impl Message {
-    fn new(time: SystemTime, channel: u8, tom_id: u32, velocity: f32) -> Self {
-        Self {
-            time,
-            note: tom_id as u8 + 36,
-            channel,
-            // Turn -0.1259 & 0.827 to 0 ... 127
-            velocity: ((1.0 - velocity.abs()) * 127.0) as u8,
-        }
+impl Hit {
+    fn to_midi_bytes(&self, channel: u8) -> [u8; 3] {
+        let note = self.tom_id as u8 + 36;
+        let velocity = ((1.0 - self.velocity.abs()) * 127.0) as u8;
+
+        [ channel, note, velocity ]
     }
 }
 
@@ -34,36 +26,25 @@ impl Message {
  */
 #[derive(Debug)]
 struct Tom {
-    id: u32,
-    hit: Option<SystemTime>,
-    velocity: Option<f32>,
+    id: u8,
+    hit: bool,
 }
 
 impl Tom {
-    fn new(id: u32) -> Self {
-        Self { id, hit: None, velocity: None }
+    fn new(id: u8) -> Self {
+        Self { id, hit: false }
     }
 
     // Remember if we're hit (so we can output next velocity event)
-    fn hit(&mut self, time: SystemTime) {
-        self.hit = Some(time);
+    fn hit(&mut self) {
+        self.hit = true;
     }
-    fn release(&mut self, time: SystemTime) -> Option<Message> {
-        if let Some(velocity) = self.velocity {
-            // 0x80 = note off on channel 1; 0x81 is note off on channel 2
-            Some(Message::new(time, 0x80, self.id, velocity))
-        } else {
-            None
-        }
-    }
-    fn record_velocity(&mut self, velocity: f32, time: SystemTime) -> Option<Message> {
-        self.velocity = Some(velocity);
-
-        // If we were hit, output note on message
-        if let Some(_time_of_hit) = self.hit {
-            self.hit = None;
+    fn record_velocity(&mut self, velocity: f32) -> Option<Hit> {
+        // If we were hit, output note on hit
+        if self.hit {
+            self.hit = false;
             // 0x90 = note on on channel 1; 0x91 is note on on channel 2
-            Some(Message::new(time, 0x90, self.id, velocity))
+            Some(Hit { tom_id: self.id, velocity })
         } else {
             None
         }
@@ -78,16 +59,12 @@ struct Drums {
 }
 
 impl Drums {
-    fn process_event(&mut self, event: gilrs::EventType, time: SystemTime) -> Option<Message> {
+    fn process_event(&mut self, event: gilrs::EventType) -> Option<Hit> {
         match event {
             gilrs::EventType::ButtonPressed(_, code) => {
                 let index = code.into_u32() - 65824;
-                self.toms[index as usize].hit(time);
+                self.toms[index as usize].hit();
                 None
-            },
-            gilrs::EventType::ButtonReleased(_, code) => {
-                let index = code.into_u32() - 65824;
-                self.toms[index as usize].release(time)
             },
             gilrs::EventType::AxisChanged(_, velocity, code) => {
                 let weird_index = code.into_u32() - 196608;
@@ -98,7 +75,7 @@ impl Drums {
                     6 => 2,
                     _ => 0,
                 };
-                self.toms[index as usize].record_velocity(velocity, time)
+                self.toms[index as usize].record_velocity(velocity)
             }
             _ => None,
         }
@@ -112,6 +89,7 @@ struct Processor {
     // Gilrs contains a raw pointer to a udev_monitor, wrap it for thread safety
     gilrs: Arc<Mutex<Gilrs>>,
     drums: Drums,
+    cache: Vec<(u64, Hit)>,
     output: jack::Port<jack::MidiOut>,
 }
 
@@ -122,15 +100,9 @@ impl Processor {
         };
 
         let gilrs = Arc::new(Mutex::new(Gilrs::new().unwrap()));
-        
-        // Iterate over all connected gamepads
-        for (_id, gamepad) in gilrs.lock().unwrap().gamepads() {
-            println!("{} is {:?}", gamepad.id(), gamepad.name());
-        }
-
         let output = client.register_port("output", jack::MidiOut::default()).unwrap();
 
-        Self { gilrs, drums, output }
+        Self { gilrs, drums, cache: vec![], output }
     }
 }
 
@@ -139,23 +111,51 @@ unsafe impl Send for Processor {}
 unsafe impl Sync for Processor {}
 
 impl jack::ProcessHandler for Processor {
-    fn process(&mut self, _: &jack::Client, process_scope: &jack::ProcessScope) -> jack::Control {
-
+    fn process(&mut self, client: &jack::Client, process_scope: &jack::ProcessScope) -> jack::Control {
+        // Get output midi port writer & transport info
         let mut writer = self.output.writer(process_scope);
+        let (_, pos) = client.transport_query();
+        let cycle_times = process_scope.cycle_times().unwrap();
+
+        let mut output: Vec<(u32, [u8; 3])> = vec![];
 
         while let Some(Event { id: _, event, time }) = self.gilrs.lock().unwrap().next_event() {
-            if let Some(message) = self.drums.process_event(event, time) {
+
+            if let Some(hit) = self.drums.process_event(event) {
                 // Get some information about our current cycle & message
-                let message_usecs_ago = SystemTime::now().duration_since(message.time).unwrap().as_micros();
-                let period_usecs = process_scope.cycle_times().unwrap().period_usecs;
+                let hit_usecs_ago = SystemTime::now().duration_since(time).unwrap().as_micros();
 
-                // Calculate when message occurred, send out midi
-                let message_frames_ago = ((message_usecs_ago as f32 / period_usecs as f32) * process_scope.n_frames() as f32) as u32;
-                let message_frame = process_scope.n_frames() - message_frames_ago;
+                // Calculate when event occurred in jack time, send out midi
+                let hit_frames_ago = ((hit_usecs_ago as f32 / cycle_times.period_usecs as f32) * process_scope.n_frames() as f32) as u32;
+                let hit_frame = process_scope.n_frames() - hit_frames_ago;
 
-                // TODO - System time can go all over the place, so this can fail..
-                writer.write(&jack::RawMidi { time: message_frame, bytes: &[ message.channel, message.note, message.velocity ] });
+                // Output note on message
+                output.push((hit_frame, hit.to_midi_bytes(0x90)));
+
+                // Cache note off message that will trigger after 1 beat, calculated from jack transport
+                let beat_usecs = ((60.0 / pos.beats_per_minute) * 1000_000.0) as u64;
+                let note_off_usec = cycle_times.current_usecs - hit_usecs_ago as u64 + beat_usecs;
+                self.cache.push((note_off_usec, hit));
             }
+        }
+
+        // Output cached notes that have to be output
+        self.cache.retain(|(usec, hit)| {
+            let should_output = *usec >= cycle_times.current_usecs && *usec < cycle_times.next_usecs;
+
+            if should_output {
+                let frame = client.time_to_frames(*usec) - cycle_times.current_frames;
+
+                output.push((frame, hit.to_midi_bytes(0x80)));
+            }
+
+            ! should_output
+        });
+
+        // Output all the things we have to output, sorted by time as jack will crash when
+        output.sort_by(|(a, _), (b, _)| a.partial_cmp(b).unwrap());
+        for (frame, bytes) in output {
+            writer.write(&jack::RawMidi { time: frame, bytes: &bytes });
         }
 
         jack::Control::Continue
@@ -167,7 +167,7 @@ fn main() {
 
     // Add processhandler & start client
     let processor = Processor::new(&client);
-    let _active_client = client.activate_async((), processor);
+    let _active_client = client.activate_async((), processor, ());
 
     // Wait for user to input string (to not exit)
     let mut user_input = String::new();
